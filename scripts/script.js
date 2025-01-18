b526fcd8c8304d293803bfceb72a5c4b051d75d2
Wendell Matheus <wendell.araujo@dcx.ufpb.br>
2025-01-18 13:34:36 -0300

    add elements form selection js

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+// Seleção dos elementos do formulário
+const form = document.querySelector('form');
+const emailInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
